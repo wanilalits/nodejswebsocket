133006dc511412baa06c6c6f2b6a6ee6c3554deb
wanilalits <wanilalits@gmail.com>
2024-11-09 11:42:39 +0530

    #

--- app.js
+++ app.js
@@ -1,18 +1,8 @@
-import express from 'express'
+const express = require('express')
+const app = express()
 
-//const express = require("express");
-const app = express();
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
 
-var http = require('http')
-var data =[
-    {name :'Lalit1'}
-]
-
-let a = http.createServer(function(req,res){
-    res.writeHead(200,{'Contant-Type':'application\json'})
-    res.write(JSON.stringify(data))
-    res.end()
-
-}).listen(3001)
-
-module.exports = app.a;
+app.listen(3000)
