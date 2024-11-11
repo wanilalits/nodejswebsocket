ffcadeefb411897936e9b4170df92f5b4a36f351
wanilalits <wanilalits@gmail.com>
2024-11-11 17:27:02 +0530

    #

--- app.js
+++ app.js
@@ -3,21 +3,21 @@ const app = express()
 const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 8080 }); // Initialize WebSocket server
 
-//wss.on('connection', function connection(ws) {
+wss.on('connection', function connection(ws) {
  
-    //ws.on('message', function message(data) {
-  //  console.log('received: %s', data);
-  //  ws.send('Lalit');
-  //});
+    ws.on('message', function message(data) {
+    console.log('received: %s', data);
+  ws.send('Lalit');
+  });
 
-  //ws.send('Lalit1');
-//});
+  ws.send('Lalit1');
+});
 
 
 
 
 app.get('/', function (req, res) {
-  res.send('Hello World..ok..')
+  res.send('Hello World..ok--')
 })
 
 app.listen(3000)
