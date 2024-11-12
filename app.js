36553d40646128b3f18c823b1639adb544a19be1
wanilalits <wanilalits@gmail.com>
2024-11-12 14:29:08 +0530

    #

--- app.js
+++ app.js
@@ -8,7 +8,17 @@ const httpServer = app.listen(process.env.PORT)
 
 
 const ws = require('ws');
-const wss = new ws.Server({ noServer: true  }); // Initialize WebSocket server  port: 3001
+const wss = new ws.Server({  port: 3001 }); // Initialize WebSocket server noServer: true 
+
+
+
+httpServer.on('upgrade', (req, socket, head) => {
+  wss.handleUpgrade(req, socket, head, (ws) => {
+    
+    wss.emit('connection', ws, req)
+  })
+  console.log('dzhb')
+})
 
 
 wss.on('connection', function connection(ws) {
