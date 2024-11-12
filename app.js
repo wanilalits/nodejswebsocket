244002b56a9cebecafc2302d83cc48a190f27de9
wanilalits <wanilalits@gmail.com>
2024-11-12 19:17:56 +0530

    #

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const app = express();
 const cors =require ('cors')
 app.use(cors());
 
-const httpServer = app.listen(3000 )
+const httpServer = app.listen( process.env.PORT )
 
 const ws = require('ws');
-const wss = new ws.Server({ port: 3001 });  // Initialize WebSocket server noServer: true  port: 3001
+const wss = new ws.Server({noServer: true});  // Initialize WebSocket server noServer: true  port: 3001
 
 CLIENTS=[];
 
